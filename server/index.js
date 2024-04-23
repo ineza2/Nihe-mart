38186e264c08cfedd840cc323e8cedc8a208e95c
ineza2 <ineza2ineza@gmail.com>
2024-04-23 20:23:48 +0200

    some changes

--- server/index.js
+++ server/index.js
@@ -17,6 +17,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use("/auth", authRouter);
 app.use("/account", accountRouter);
-app.listen(5000, () => {
-  console.log("Server started on port 5000");
+app.listen(3000, () => {
+  console.log("Server started on port 3000");
 });
